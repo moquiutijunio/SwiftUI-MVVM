a348c6de7d157eb53a694812bdcaead01c272a42
Junio Cesar Moquiuti <idd_jmoquiuti@NI-INV2549.uolcorp.intranet>
2021-09-21 16:50:31 -0400

    first commit

---
+++ swiftui-mvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-mvvm
+//
+//  Created by Junio Cesar Moquiuti on 21/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
